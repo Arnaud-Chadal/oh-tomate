6235d662435a6749db53c3db425497194ebbc246
Arnaud-Chadal <arnaud.chadal@efrei.net>
2025-03-12 18:51:21 +0100

    Original commit

---
+++ pythonFiles/automate.py
@@ -0,0 +1,36 @@
+class automate :
+
+    def __init__(self, alphabet, ) :
+        self.alphabet = alphabet
+        self.nodeList = []
+
+# Méthode de vérification :
+
+    def estMinimise(self) :
+        pass
+
+    def estStandard(self) :
+        pass
+
+    def estDeterministe(self) :
+        pass
+
+    def estComplet(self) :
+        pass
+
+# Méthodes d'action :
+
+    def determiniser(self) :
+        pass
+
+    def minimiser(self) :
+        pass
+
+    def completer(self) :
+        pass
+
+    def complementer(self) :
+        pass
+
+    def standardise(self) :
+        pass
---
+++ pythonFiles/main.py
@@ -0,0 +1,28 @@
+# class main :
+#     def __init__() :
+#         pass
+
+
+#     def loadAutomate() :
+#         file = open("temoin.txt", "r")
+#         nbr = 0
+#         fileContent = file.split("\n")
+#         print(fileContent)
+#         nbr = 0
+        
+
+
+
+#     def run() :
+#         pass
+
+
+    
+
+file = open("./automates/temoin.txt")
+fileContent = file.read()
+fileContent = fileContent.split("\n")
+for i in range(len())
+
+print(fileContent)
+nbr = 0
