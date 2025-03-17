85fece801209326c8ee432f82cf2da5823521c86
Mathis-Desbonnet <mathis.desbonnet@gmail.com>
2025-03-17 09:54:27 +0100

    Can rotate the arrow around a node + can click on a arrow to select it -> Problem : Have to make an algo that seperate each arrow (no arrow on an other one)

--- graphicLink.py
+++ graphicLink.py
@@ -1,36 +1,95 @@
 import pygame
 from math import cos, sin, atan2, acos, sqrt, pow, pi
 
-class GraphicLink :
+
+class GraphicLink:
 
     def __init__(self, linkVar, nodeVar):
-        self.my_font = pygame.font.SysFont('Comic Sans MS', 30)
+        self.my_font = pygame.font.SysFont("Comic Sans MS", 30)
         self.linkVar = linkVar
         self.nodeVar = nodeVar
-        self.collision = pygame.rect.Rect(nodeVar.x+32, nodeVar.y+32, 30, 5)
         self.image = pygame.image.load("./images/link.png").convert_alpha()
         self.image2 = pygame.image.load("./images/linkClick.png").convert_alpha()
         self.rect = self.image.get_rect()
+        self.isClicked = False
+        self.collision = self.image.get_rect()
 
-# Problème : l'objet link ne connait pas la position du noeud de destination puisque la transition contient un node et pas un graphicNode ----> Solved !
-#idée : utiliser le cos et sin pour faire tourner la flèche autour du noeud quand on le déplace
-    def draw(self, screen) :
-        pygame.draw.line(screen, (255, 255, 255), (self.nodeVar.x+32, self.nodeVar.y+32), (self.linkVar[1].x+32, self.linkVar[1].y+32), 5)
-        text_surface = self.my_font.render(self.linkVar[0], False, (255, 255, 255))
-        screen.blit(text_surface, (self.nodeVar.x+(self.linkVar[1].x-self.nodeVar.x)/2, self.nodeVar.y+(self.linkVar[1].y-self.nodeVar.y)/2))
+    # Problème : l'objet link ne connait pas la position du noeud de destination puisque la transition contient un node et pas un graphicNode ----> Solved !
+    # idée : utiliser le cos et sin pour faire tourner la flèche autour du noeud quand on le déplace ----> Solved !
+    def draw(self, screen):
         rad = self.calculateDeltaImage()
-        imageRotate = pygame.transform.rotate(self.image, rad*180/pi)
-        self.rect = imageRotate.get_rect(center = self.rect.center)
-        xM = self.rect.x - (self.linkVar[1].x+32)
-        yM = self.rect.y - (self.linkVar[1].y+32)
-        #screen.blit(imageRotate, (self.rect.x+self.linkVar[1].x-30, self.rect.y+self.linkVar[1].y+15))
-        screen.blit(imageRotate, (xM*cos(rad) + yM*sin(rad) + self.linkVar[1].x+32, (-xM * sin(rad)) + yM*cos(rad) + self.linkVar[1].y+32))
-    
+        rectArc = pygame.rect.Rect(
+            self.nodeVar.x + 32,
+            self.nodeVar.y + 32,
+            sqrt(
+                pow((self.linkVar[1].x - self.nodeVar.x), 2)
+                + (pow((self.linkVar[1].y - self.nodeVar.y), 2))
+            ),
+            5,
+        )
+        pygame.draw.rect(screen, (255, 0, 0), rectArc)
+        pygame.draw.rect(screen, (255, 255, 0), self.collision)
+        rectArc.bottom = self.linkVar[1].x + 32
+        rectArc.right = self.linkVar[1].y + 32
+        pygame.draw.arc(screen, (255, 255, 255), rectArc, pi, pi / 3)
+        pygame.draw.line(
+            screen,
+            (255, 255, 255),
+            (self.nodeVar.x + 32, self.nodeVar.y + 32),
+            (self.linkVar[1].x + 32, self.linkVar[1].y + 32),
+            5,
+        )
+        text_surface = self.my_font.render(self.linkVar[0], False, (255, 255, 255))
+        screen.blit(
+            text_surface,
+            (
+                self.nodeVar.x + (self.linkVar[1].x - self.nodeVar.x) / 2,
+                self.nodeVar.y + (self.linkVar[1].y - self.nodeVar.y) / 2,
+            ),
+        )
+        if self.isClicked:
+            imageRotate = pygame.transform.rotate(self.image2, rad * 180 / pi)
+        else:
+            imageRotate = pygame.transform.rotate(self.image, rad * 180 / pi)
+        self.collision = imageRotate.get_rect(center=self.rect.center)
+        self.collision.x = (-(32 + 15) * cos(-rad)) + self.linkVar[1].x + 15
+        self.collision.y = (-(32 + 15) * sin(-rad)) + self.linkVar[1].y + 15
+        self.rect = imageRotate.get_rect(center=self.rect.center)
+        screen.blit(
+            imageRotate,
+            (
+                (-(32 + 15) * cos(-rad)) + self.linkVar[1].x + 15,
+                (-(32 + 15) * sin(-rad)) + self.linkVar[1].y + 15,
+            ),
+        )
+
     def calculateDeltaImage(self):
-        if (((sqrt(pow((self.linkVar[1].x-self.nodeVar.x), 2)+(pow((self.linkVar[1].y-self.nodeVar.y),2)))/1000000)*1000000) == 0):
+        if (
+            (
+                sqrt(
+                    pow((self.linkVar[1].x - self.nodeVar.x), 2)
+                    + (pow((self.linkVar[1].y - self.nodeVar.y), 2))
+                )
+                / 1000000
+            )
+            * 1000000
+        ) == 0:
             return 0
-        value = (((self.linkVar[1].x)-(self.nodeVar.x))/((sqrt(pow((self.linkVar[1].x-self.nodeVar.x), 2)+(pow((self.linkVar[1].y-self.nodeVar.y),2)))/1000000)*1000000)/10000)*10000
+        value = (
+            ((self.linkVar[1].x) - (self.nodeVar.x))
+            / (
+                (
+                    sqrt(
+                        pow((self.linkVar[1].x - self.nodeVar.x), 2)
+                        + (pow((self.linkVar[1].y - self.nodeVar.y), 2))
+                    )
+                    / 1000000
+                )
+                * 1000000
+            )
+            / 10000
+        ) * 10000
         rad = acos(value)
-        if (self.nodeVar.y < self.linkVar[1].y):
+        if self.nodeVar.y < self.linkVar[1].y:
             return -rad
         return rad
--- graphicWindow.py
+++ graphicWindow.py
@@ -1,12 +1,13 @@
 import pygame
 import graphicNode
 import graphicLink
+from math import pi
 
 
-class Main :
-    def __init__(self, nodeList) -> None :
+class Main:
+    def __init__(self, nodeList) -> None:
         pygame.font.init()
-        self.my_font = pygame.font.SysFont('Comic Sans MS', 30)
+        self.my_font = pygame.font.SysFont("Comic Sans MS", 30)
         self.screen = pygame.display.set_mode((1920, 1080))
         self.running = True
         self.nodeList = []
@@ -14,68 +15,86 @@ class Main :
         self.nodeAddressToGraphicNodeAddress = {}
         self.clicked = None
         self.grabbed = None
+        self.xMousePos, self.yMousePos = pygame.mouse.get_pos()
         nbr = 0
-        for graphNode in nodeList :
-            graphicNo = graphicNode.GraphicNode(nbr*200, 200, graphNode)
+        for graphNode in nodeList:
+            graphicNo = graphicNode.GraphicNode(nbr * 200, 200, graphNode)
             self.nodeList.append(graphicNo)
             self.nodeAddressToGraphicNodeAddress[graphNode] = graphicNo
             nbr += 1
         nbr = 0
-        for graphNode in nodeList :
+        for graphNode in nodeList:
             self.linkList.append([])
-            for link in graphNode.linkList :
-                self.linkList[nbr].append(graphicLink.GraphicLink([link[0], self.nodeAddressToGraphicNodeAddress[link[1]]], self.nodeList[nbr]))
+            for link in graphNode.linkList:
+                self.linkList[nbr].append(
+                    graphicLink.GraphicLink(
+                        [link[0], self.nodeAddressToGraphicNodeAddress[link[1]]],
+                        self.nodeList[nbr],
+                    )
+                )
                 print(self.linkList[nbr][0].linkVar[1])
             nbr += 1
-            
 
-
-
-    def run(self) :
-        while self.running : 
-            #Check des events
+    def run(self):
+        while self.running:
+            self.screen.fill((0, 0, 0))
+            # Check des events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 :
-                    for graphNode in self.nodeList :
-                        if graphNode.collision.collidepoint(event.pos) :
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    for graphNode in self.nodeList:
+                        if graphNode.collision.collidepoint(event.pos):
                             self.grabbed = graphNode
                             self.clicked = graphNode
-                elif event.type == pygame.MOUSEBUTTONUP and event.button == 1 :
+                    for groups in self.linkList:
+                        for link in groups:
+                            link.isClicked = False
+                            if (
+                                link.collision.collidepoint(event.pos)
+                                and self.grabbed == None
+                            ):
+                                print("Oui !")
+                                link.isClicked = True
+                                self.clicked = link
+                                self.grabbed = link
+                elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
                     self.clicked = self.grabbed
                     self.grabbed = None
-                    
-            self.screen.fill((0, 0, 0))
-                    
+
             for linkGroup in self.linkList:
                 for links in linkGroup:
                     links.draw(self.screen)
-            
+
             self.linkList[0][0].calculateDeltaImage()
 
-            #Affichage des noeuds
-            for graphNode in self.nodeList :
-                if self.grabbed == graphNode :
-                    graphNode.setPos(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
-                if self.clicked == graphNode :
+            # Affichage des noeuds
+            for graphNode in self.nodeList:
+                if self.grabbed == graphNode:
+                    graphNode.setPos(
+                        pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]
+                    )
+                if self.clicked == graphNode:
                     self.screen.blit(graphNode.image2, (graphNode.x, graphNode.y))
-                else : self.screen.blit(graphNode.image, (graphNode.x, graphNode.y))
-                text_surface = self.my_font.render(graphNode.nodeVar.name, False, (0, 0, 0))
-                self.screen.blit(text_surface, (graphNode.x+15, graphNode.y+15))
-                
+                else:
+                    self.screen.blit(graphNode.image, (graphNode.x, graphNode.y))
+                text_surface = self.my_font.render(
+                    graphNode.nodeVar.name, False, (0, 0, 0)
+                )
+                self.screen.blit(text_surface, (graphNode.x + 15, graphNode.y + 15))
+
                 # affichage des lignes
-                #idée : afficher des arcs plutôt que des lignes pour pas que ça se superpose
-                #pour ça, compter le nombre de transition d'un noeud vers un autre et donner des coefs de courbure selon ce nombre à intervalles réguliers
+                # idée : afficher des arcs plutôt que des lignes pour pas que ça se superpose
+                # pour ça, compter le nombre de transition d'un noeud vers un autre et donner des coefs de courbure selon ce nombre à intervalles réguliers
                 # for link in graphNode.nodeVar.linkList :
                 #     for graphNode2 in self.nodeList :
                 #         if graphNode2.nodeVar == link[1] :
                 #             pygame.draw.line(self.screen, (255, 255, 255), (graphNode.x+32, graphNode.y+32), (graphNode2.x+32, graphNode2.y+32), 5)
                 #             text_surface = self.my_font.render(link[0], False, (255, 255, 255))
                 #             self.screen.blit(text_surface, (graphNode.x+(graphNode2.x-graphNode.x)/2, graphNode.y+(graphNode2.y-graphNode.y)/2))
-            
-                #Affichage des flèches de transitions pour indiquer le sens
-                #On ne peut pas juste afficher un sprite car il faudra pouvoir cliquer dessus si on veut supprimer la transition
+
+                # Affichage des flèches de transitions pour indiquer le sens
+                # On ne peut pas juste afficher un sprite car il faudra pouvoir cliquer dessus si on veut supprimer la transition
                 # for links in self.linkList :
                 #     for link in links :
                 #         link.draw(self.screen)
